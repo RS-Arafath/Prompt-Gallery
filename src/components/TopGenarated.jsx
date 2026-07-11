073aebf62e415324bdd4e3a6e5214fee65de9c2e
Arafath <rsarafath.signin@gmail.com>
2026-07-12 02:45:46 +0600

    Update download counts and adjust cache strategy in TopGenerated component

--- src/components/TopGenarated.jsx
+++ src/components/TopGenarated.jsx
@@ -1,12 +1,11 @@
-
-import Image from "next/image";
-import { Download, Heart } from "lucide-react";
-import LikeButton from "./shared/LikeButton";
-import { Button } from "@heroui/react";
+import Image from 'next/image';
+import { Download } from 'lucide-react';
+import LikeButton from './shared/LikeButton';
+import { Button } from '@heroui/react';
 
 const TopGenerated = async () => {
-  const res = await fetch("https://promptgallery-ai.vercel.app/data.json", {
-    cache: "force-cache",
+  const res = await fetch('https://promptgallery-ai.vercel.app/data.json', {
+    cache: 'no-cache',
   });
 
   const data = await res.json();
@@ -27,7 +26,7 @@ const TopGenerated = async () => {
             key={item.id}
             className="flex h-full flex-col overflow-hidden rounded-2xl border border-gray-200 bg-white shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
           >
-            <div className="relative aspect-[3/3.5] cursor-pointer overflow-hidden bg-neutral-950">
+            <div className="relative  aspect-[3/3.5] cursor-pointer overflow-hidden bg-neutral-950">
               {/* Background */}
               <Image
                 src={item.imageUrl}
@@ -44,6 +43,7 @@ const TopGenerated = async () => {
                 src={item.imageUrl}
                 alt={item.title}
                 fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="rounded-2xl object-fill p-3 transition-transform duration-300 hover:scale-115"
               />
             </div>
@@ -53,7 +53,7 @@ const TopGenerated = async () => {
               <h3 className="line-clamp-2 min-h-[56px] font-bold text-lg text-slate-700">
                 {item.title}
               </h3>
-<div className="border mt-3"></div>
+              <div className="border mt-3"></div>
               <div className="mt-3 sm:mt-4 flex items-center justify-between text-sm text-gray-600">
                 <div className="flex items-center gap-2">
                   <Download size={18} />
