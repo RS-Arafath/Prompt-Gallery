66e7137cf4092d4ab56f84a0aa01ceba5f67eeb7
Arafath <rsarafath.signin@gmail.com>
2026-07-21 01:26:23 +0600

    feat: make Category component asynchronous to load data from JSON file

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Category = () => {
+const Category = async() => {
+   const filePath = path.join(process.cwd(), 'public', 'category.json');
+      const fileContents = await fs.readFile(filePath, 'utf-8');
+      const data = JSON.parse(fileContents);
+      const category = data;
   return (
     <div>
       
